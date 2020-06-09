c4aa5d60effb5a2a24326b678377b1e9995aa9be
Brian <bburger@students.mchenry.edu>
2020-06-09 12:36:52 -0500

    Log Fragment Added

--- app/src/main/java/com/ebookfrenzy/pinballappmad155/MainActivity.kt
+++ app/src/main/java/com/ebookfrenzy/pinballappmad155/MainActivity.kt
@@ -2,10 +2,38 @@ package com.ebookfrenzy.pinballappmad155
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val playLog = PlayLogFragment()
+        val findLocation = FindLocationFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, playLog)
+            commit()
+        }
+
+        btnLog.setOnClickListener {
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, playLog)
+                addToBackStack(null)
+                commit()
+            }
+
+        }
+
+        btnFind.setOnClickListener {
+
+            supportFragmentManager.beginTransaction().apply {
+                replace(R.id.flFragment, findLocation)
+                addToBackStack(null)
+                commit()
+            }
+        }
+
     }
 }
